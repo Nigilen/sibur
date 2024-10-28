69c615352def8a2d6a472998f5a65e6340c05802
nigilen <you@example.com>
2024-10-28 13:35:47 +0100

    fix(#3): fix wrap text for pride section on mob

--- src/components/sections/about/pride/pride.tsx
+++ src/components/sections/about/pride/pride.tsx
@@ -7,7 +7,7 @@ export const Pride = () => {
       <div className={styles.pride_content}>
         <h3 className={styles.pride_title}>Мы&nbsp;<span className={styles.pride_title_decor}>гордимся</span></h3>
         <p className={styles.pride_description}>
-          Результатами выпускников наших<br/>образовательных проектов, целью которых<br/>является создание инноваций и развитие<br/>российской экономики
+          Результатами выпускников наших <br className={styles.br}/>образовательных проектов, целью которых <br className={styles.br}/>является создание инноваций и&nbsp;развитие <br className={styles.br}/>российской экономики
         </p>
       </div>
       <Image className={styles.pride_img} src="/praid.png" alt="" width={808} height={569} quality={100}/>
