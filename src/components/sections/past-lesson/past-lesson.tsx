678567bac7a8157a6fccfb59d1049060dc691196
nigilen <you@example.com>
2024-10-29 17:55:43 +0200

    feat(#3): add api

--- src/components/sections/past-lesson/past-lesson.tsx
+++ src/components/sections/past-lesson/past-lesson.tsx
@@ -1,4 +1,4 @@
-import { Video } from '@/src/ui-kit/video/video';
+// import { Video } from '@/src/ui-kit/video/video';
 import { Gallery } from './gallery/gallery';
 import styles from './past-lesson.module.css';
 
@@ -9,7 +9,7 @@ export const PastLesson = () => {
       <p className={styles.lesson_description}>Смотрите, как школьники <br className={styles.br}/>открывают для себя мир науки <br className={styles.br}/>через увлекательный квест<br className={styles.br}/>и&nbsp;опыты! Такое может быть&nbsp;<br className={styles.br}/>и&nbsp;у&nbsp;вас — оставьте заявку <br className={styles.br}/>и пригласите СИБУР&nbsp;<br className={styles.br}/>в&nbsp;вашу&nbsp;школу!</p>
 
 
-      <div className={styles.video_decor}>
+      {/* <div className={styles.video_decor}>
         <div className={styles.video_decor_blue_line}></div>
         <div className={styles.video_decor_red_line}></div>
         <div className={styles.video_decor_star}></div>
@@ -20,7 +20,7 @@ export const PastLesson = () => {
           preloader={'/lesson-video-preloader.png'} 
           classNames={styles.lesson_video}
         />
-      </div>
+      </div> */}
 
       <Gallery />
 
