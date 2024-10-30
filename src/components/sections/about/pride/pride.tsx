59df5342fd4a3a4fccf3f093c41aea78879168c3
nigilen <you@example.com>
2024-10-30 14:40:52 +0200

    fix(#3): fix wrap pride text

--- src/components/sections/about/pride/pride.tsx
+++ src/components/sections/about/pride/pride.tsx
@@ -7,7 +7,7 @@ export const Pride = () => {
       <div className={styles.pride_content}>
         <h3 className={styles.pride_title}>Мы&nbsp;<span className={styles.pride_title_decor}>гордимся</span></h3>
         <p className={styles.pride_description}>
-          Результатами выпускников наших <br className={styles.br}/>образовательных проектов, целью которых <br className={styles.br}/>является создание инноваций и&nbsp;развитие <br className={styles.br}/>российской экономики
+          Результатами выпускников <br className={styles.br_1440}/>наших образовательных <br className={styles.br_1440}/>проектов, целью которых <br className={styles.br_1440}/>является создание инноваций и&nbsp;развитие российской экономики
         </p>
       </div>
       <Image className={styles.pride_img} src="/praid.png" alt="" width={808} height={569} quality={100}/>
